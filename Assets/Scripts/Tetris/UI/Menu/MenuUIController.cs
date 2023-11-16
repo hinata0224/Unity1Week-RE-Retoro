fee0487526ef1fa2ad3566a5d3c08c86c9517918
Sora(HinataSato) <hinata.act.2@gmail.com>
2023-11-17 06:27:46 +0900

    Null参照の修正

--- Assets/Scripts/Tetris/UI/Menu/MenuUIController.cs
+++ Assets/Scripts/Tetris/UI/Menu/MenuUIController.cs
@@ -2,24 +2,40 @@ using UnityEngine;
 using UniRx;
 using System;
 using UnityEngine.UI;
+using SceneController;
 
 namespace Tetris_UI
 {
     public class MenuUIController : MonoBehaviour
     {
-        [SerializeField]
-        private GameObject Menu;
-        [SerializeField]
-        private Button _openButton;
+        [SerializeField] private GameObject Menu;
+        [SerializeField] private Button _openButton;
+        [SerializeField] private Button _titleButton;
+        [SerializeField] private Button _moreButton;
+
         private bool isOpneMenu = true;
 
         private Subject<bool> openMenu = new();
         public IObservable<bool> IsOpenMenu => openMenu;
+        private SceneMangerController _sceneManger;
+
+        private void Awake()
+        {
+            _sceneManger = SceneMangerController.Instance;
+        }
 
         public void Initializ()
         {
             _openButton.OnButtonObservable()
                 .Subscribe(_ => DisplayMenu())
+                .AddTo(gameObject);
+
+            _titleButton.OnButtonObservable()
+                .Subscribe(_ => _sceneManger.GoTitle())
+                .AddTo(gameObject);
+
+            _moreButton.OnButtonObservable()
+                .Subscribe(_ => _sceneManger.GoTetrisStart())
                 .AddTo(gameObject);
         }
 
--- Assets/Scripts/Tetris/UI/Rezult/ResultView.cs
+++ Assets/Scripts/Tetris/UI/Rezult/ResultView.cs
@@ -1,26 +1,41 @@
 using UnityEngine;
 using TMPro;
 using UniRx;
+using UnityEngine.UI;
+using SceneController;
 
 namespace Tetris_UI
 {
     public class ResultView : MonoBehaviour
     {
-        [SerializeField]
-        private GameObject rezult;
-
-        [SerializeField]
-        private TextMeshProUGUI scoreText;
+        [SerializeField] private GameObject rezult;
+        [SerializeField] private TextMeshProUGUI scoreText;
+        [SerializeField] private Button _titleButton;
+        [SerializeField] private Button _moreButton;
 
         private ScoreModel model = new();
 
         private static Subject<Unit> gameOver = new();
+        private SceneMangerController _sceneManger;
+
+        private void Awake()
+        {
+            _sceneManger = SceneMangerController.Instance;
+        }
 
         private void Start()
         {
             gameOver.First()
                 .Subscribe(x => GameOver())
                 .AddTo(this);
+
+            _titleButton.OnButtonObservable()
+                .Subscribe(_ => _sceneManger.GoTitle())
+                .AddTo(gameObject);
+
+            _moreButton.OnButtonObservable()
+                .Subscribe(_ => _sceneManger.GoTetrisStart())
+                .AddTo(gameObject);
         }
 
         public void GameOver()
